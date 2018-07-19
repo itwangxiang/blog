8973a9d595b0032fb52063042ac110b79a1b0289
itwangxiang <itwangixang@foxmail.com>
2018-07-19 13:38:50 +0800

    sh

--- nginx.sh
+++ nginx.sh
@@ -3,5 +3,8 @@ if [ ${PWD##*/} != "docs" ];
   echo "please run at docs."
   exit 2
 fi
+echo "update git code start..."
+git pull origin master
+echo "update git code done."
 rm -rf ../../nginx/webroot/asset
 cp -R asset/* ../../nginx/webroot/asset
