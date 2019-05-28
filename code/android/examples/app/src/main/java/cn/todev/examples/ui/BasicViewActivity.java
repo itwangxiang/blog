b73ac301417c4dffe6fa769ca45f0d71a867c94d
itwangxiang <itwangxiang@foxmail.com>
2019-05-28 21:22:05 +0800

    m

--- code/android/examples/app/src/main/java/cn/todev/examples/ui/BasicViewActivity.java
+++ code/android/examples/app/src/main/java/cn/todev/examples/ui/BasicViewActivity.java
@@ -2,25 +2,34 @@ package cn.todev.examples.ui;
 
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.graphics.PixelFormat;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.text.TextUtils;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.blankj.utilcode.util.ScreenUtils;
 import com.blankj.utilcode.util.ToastUtils;
+import com.orhanobut.logger.Logger;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.todev.examples.R;
 import cn.todev.examples.widget.CircleView;
+
+import static android.view.Gravity.RIGHT;
 
 public class BasicViewActivity extends AppCompatActivity {
 
@@ -148,7 +157,7 @@ public class BasicViewActivity extends AppCompatActivity {
         tvInfo.setText(info);
     }
 
-    @OnClick({R.id.cv_hello, R.id.btn_scroll_to, R.id.btn_scroll_by, R.id.btn_refresh, R.id.btn_animator, R.id.btn_animate_change_width})
+    @OnClick({R.id.cv_hello, R.id.btn_scroll_to, R.id.btn_scroll_by, R.id.btn_refresh, R.id.btn_animator, R.id.btn_animate_change_width, R.id.btn_show_window})
     public void onViewClicked(View view) {
         int scrollX = 0;
         int scrollY = 0;
@@ -188,6 +197,59 @@ public class BasicViewActivity extends AppCompatActivity {
             case R.id.btn_refresh:
                 refreshLocaltion();
                 break;
+            case R.id.btn_show_window:
+                openWindow();
+                break;
         }
     }
+
+    public void openWindow() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            ToastUtils.showLong("请授权悬浮权限");
+            return;
+        }
+
+        WindowManager windowManager = getWindowManager();
+
+
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                200,
+                200,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
+                PixelFormat.TRANSLUCENT
+        );
+        params.x = 200;
+        params.y = 200;
+        params.setTitle("Load Average");
+
+        Button floatingButton = new Button(this);
+        floatingButton.setText("window");
+        floatingButton.setOnTouchListener(new View.OnTouchListener() {
+
+            int mTouchStartX,mTouchStartY;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        mTouchStartX = (int) event.getRawX();
+                        mTouchStartY = (int) event.getRawY();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        params.x += (int) event.getRawX() - mTouchStartX;
+                        params.y += (int) event.getRawY() - mTouchStartY;//相对于屏幕左上角的位置
+                        windowManager.updateViewLayout(v, params);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        break;
+                }
+
+                return false;
+            }
+        });
+
+        windowManager.addView(floatingButton, params);
+    }
+
 }
