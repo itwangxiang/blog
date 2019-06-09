06539e32094e65c348544ccdac12adb7dce2c52f
itwangxiang <itwangxiang@foxmail.com>
2019-06-09 13:01:57 +0800

    m

---
+++ code/java/src/cn/todev/examples/demo/Reflection.java
@@ -0,0 +1,26 @@
+package cn.todev.examples.demo;
+
+public class Reflection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		
+		
+		
+		
+        return null;
+    }
+
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+}
