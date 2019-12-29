1fc9c6c2ca3d3b01bd793ecc968c74ef92f25f80
极度思想 <itwangxiang@foxmail.com>
2019-12-29 21:01:58 +0800

    Update 1024crawlers.py

--- asset/1024crawlers.py
+++ asset/1024crawlers.py
@@ -3,6 +3,7 @@ import re
 import os
 import json
 from lxml import etree
+from multiprocessing.pool import ThreadPool
 
 # 草榴 URL
 base_url = 'https://t66y.com/'
@@ -10,6 +11,13 @@ base_url = 'https://t66y.com/'
 dge_prefix_url = 'http://t66y.com/thread0806.php?fid=16&page='
 
 # 缓存文件
+# data.json
+# {
+#     'htm_data/2001/16/3742892.html': {
+#         'title': '',
+#         'imgs': []
+#     }
+# }
 dbJsonFile = 'data.json'
 dataJson = {}
 
@@ -26,18 +34,27 @@ headers = {
     'Cookie': '__cfduid=dccaf83180801300d8243479f98b542e91577519364; PHPSESSID=jlka2tcth2b95kq9fhr51gbd54; serverInfo=t66y.com%7C149.129.81.172; 227c9_lastvisit=0%091577526232%09%2Fnotice.php%3F'
 }
 
+# 读取数据
+
+
+def readJson():
+    global dataJson
+    if os.path.exists(dbJsonFile) == True:
+        with open(dbJsonFile, 'r') as f:
+            dataJson = json.load(f)
+    print('已读取 %d 条缓存数据：' %len(dataJson))
+
+def writeJson():
+    with open(dbJsonFile, 'w') as f:
+        json.dump(dataJson, f)
+    print('%d 条数据缓存成功' %len(dataJson))
+
 # 用作目录和文件名称时不合法的字符
 invalidchars = ['?', '\\', '/', ':', '*', '<', '>', '|', '\"']
 def process_str(oristr):
     for invalidchar in invalidchars:
         oristr = oristr.replace(invalidchar, '_')
     return oristr
-
-# 爬取页面中图片列表
-def crawlerImgUrls(html):
-    tree = etree.HTML(str(html, 'gbk', errors='ignore'))
-    img_urls = tree.xpath('//img/@data-src')
-    return img_urls
 
 # 爬取页面的 Content
 def crawlerContent(url):
@@ -46,11 +63,8 @@ def crawlerContent(url):
     page_content = page_response.content
     return page_content
 
-# 爬页面中文章链接列表
-# def crawlerArticeUrls(html):
-#     article_urls = re.findall(r'<h3><a href="(.*)" target="_blank" id="">(?!<.*>).*</a></h3>', html)
-#     return article_urls
-def crawlerArticeUrls(page):
+# 爬页面中所有帖子的链接
+def crawlerArticleUrls(page):
     ##列表链接+页码
     article_url = dge_prefix_url + str(page)
     ##转为字符串
@@ -68,69 +82,95 @@ def crawlerArticeUrls(page):
     url_list = pattern_url.findall(str(com_list))
     return url_list
 
-# 爬草榴达盖尔版块的 number 个页面
-def crawlers(number):
-    print(u'开始抓取数据')
+# 爬帖子中的所有图片的链接
+def crawlerImgUrls(article):
+    article_url = article['url']
 
-    for page in range(1, number + 1):
-        print(u'Page(%d/%d): 开始抓取' %(page, number))
-        
-        page_article_urls = crawlerArticeUrls(page)
-        artice_len = len(page_article_urls)
-        
-        print(u'Page(%d/%d): 一共找到了 %d 个帖子' % (page, number, artice_len))
-        
-        idx = 0
-        for article_url in page_article_urls:
-            idx = idx + 1
+    if article_url in dataJson:
+        jsonArticle = dataJson[article_url]
+        article['title'] = jsonArticle['title']
+        article['imgs'] = jsonArticle['imgs']
+        article['status'] = 'exists'
+        return article
 
-            if article_url in dataJson and len(dataJson[article_url]['img_urls']) > 0:
-                print(u'Page(%d/%d): 第 %d/%d 个帖子，已经抓取(%d 张图片)' % (page, number, idx, artice_len, len(dataJson[article_url]['img_urls'])))
-                continue
-            else:
-                print(u'Page(%d/%d): 第 %d/%d 个帖子，开始抓取 - %s' % (page, number, idx, artice_len, article_url))
-            
-            #文章-Content
-            article_content = crawlerContent(base_url + article_url)
-            #文章-标题
-            article_title = re.findall(r'<h4>(.*)</h4>', str(article_content, 'gbk', errors='ignore'))
-            if len(article_title) > 0:
-                article_title = article_title[0]
-            else:
-                article_title = 'default'
-            #文章-图片列表
-            article_img_urls = crawlerImgUrls(article_content)
+    #文章-Content
+    article_content = crawlerContent(base_url + article['url'])
+    #文章-标题
+    article_title = re.findall(r'<h4>(.*)</h4>', str(article_content, 'gbk', errors='ignore'))
+    if len(article_title) > 0:
+        article_title = article_title[0]
+    else:
+        article_title = 'default'
+    #文章-图片
+    tree = etree.HTML(str(article_content, 'gbk', errors='ignore'))
+    article_imgs = tree.xpath('//img/@data-src')
 
-            dataJson[article_url] = {
-                "title": article_title,
-                "url": article_url,
-                "img_urls": article_img_urls
-            }
+    article['title'] = article_title
+    article['imgs'] = article_imgs
+    article['status'] = 'add'
+    return article
 
-            print(u'Page(%d/%d): 第 %d/%d 个帖子，抓取成功（%d 张图片）' % (page, number, idx, artice_len, len(article_img_urls)))
-        
-        print(u'Page(%d/%d): 抓取成功' %(page, number))
+
+# 爬草榴达盖尔版块的第 number 个页面
+def crawlerPages(page):
+    print('Page%d - 开始抓取帖子' %(page))
+
+    article_urls = crawlerArticleUrls(page)
+    article_len = len(article_urls)
+    print('Page%d - 一共找到了 %d 个帖子' % (page, article_len))
+
+    articles = []
+    idx = 0
+    for article_url in article_urls:
+        idx = idx + 1
+        articles.append({
+            'page': page,
+            'url': article_url,
+            'position': str(idx) + '/' + str(article_len)
+        })
     
-    print(u'数据抓取成功')
+    return articles
 
 if __name__ == "__main__":
     print('*' * 100)
     print('欢迎使用 1024 达盖尔图片下载小助手')
     print('*' * 100)
     number  = input('请输入需要抓取的页数:\n')
+    number = int(number)
 
-    if os.path.exists(dbJsonFile) == True:
-        with open(dbJsonFile, 'r') as f:
-            dataJson = json.load(f)
-
-    print(u'已读取 %d 条缓存数据：' %len(dataJson))
+    readJson()
 
     try:
-        crawlers(int(number))
+        articles = []
+
+        page_results = ThreadPool(7).imap_unordered(crawlerPages, range(1, number))
+        for page_articles in page_results:
+            articles = articles + page_articles
+        
+        print('/n')
+
+        results = ThreadPool(7).imap_unordered(crawlerImgUrls, articles)
+        for article in results:
+            status = article['status']
+            page = article['page']
+            position = article['position']
+            article_url = article['url']
+            article_title = article['title']
+            article_imgs = article['imgs']
+
+            if status == 'add':
+                print('Page(%d/%d) - 帖子(%s) - %s 抓取 %d 张图片成功' %(page, number, position, article_title, len(article_imgs)))
+            elif status == 'exists':
+                print('Page(%d/%d) - 帖子(%s) - %s 已缓存 %d 张图片' %(page, number, position, article_title, len(article_imgs)))
+            else:
+                print('未知错误')
+            
+            # 保持缓存数据中
+            dataJson[article_url] = article
+
     except BaseException as e:
         print(u'抓取失败 - %s' %e)
 
-    with open(dbJsonFile, 'w') as f:
-        json.dump(dataJson, f)
-    
-    print(u'%d 条数据缓存成功' %len(dataJson))
+    writeJson()
+
+    input('抓取完成:\n')
